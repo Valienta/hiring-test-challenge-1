6d1ca41435ab878989b01b1b4d1e014e1456ad21
criscapunal <crcaguilerapo@unal.edu.co>
2022-04-09 02:53:52 +0000

    config migration

--- src/connection.ts
+++ src/connection.ts
@@ -1,22 +1,8 @@
 import { createConnection, Connection } from 'typeorm';
-import config from './config';
+import ormconfig from './ormconfig';
 
 async function connect(): Promise<Connection> {
-  return createConnection({
-    type: 'postgres',
-    host: config.postgresDB.host,
-    port: config.postgresDB.port,
-    username: config.postgresDB.username,
-    password: config.postgresDB.password,
-    database: config.postgresDB.database,
-    synchronize: false,
-    subscribers: [],
-    logger: 'simple-console',
-    entities: [
-      `${__dirname}/domain/*.js`,
-      `${__dirname}/domain/*.ts`
-    ]
-  });
+  return createConnection(ormconfig);
 }
 
 export default connect;
